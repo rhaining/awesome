ef09e7735a3f334ffa78aaf400c108ceb66683ff
Rob Haining <rob@tolar.ws>
2020-02-10 10:19:53 -0500

    test referer

--- webring/script.js
+++ webring/script.js
@@ -36,14 +36,16 @@ function randomElement(array) {
 function populateWebsite(websites) {
   if(inIframe()) {
     // var parentHost = parent.window.location.hostname
-    //
-    // var website = null
-    // while(website == null) {
-    //   website = randomElement(websites)
-    //   if(parentHost.lenght > 0 && website["url"].includes(parentHost)) {
-    //     website = null
-    //   }
-    // }
+    var parentHost = document.referrer
+    console.log("parent host = " + parentHost)
+
+    var website = null
+    while(website == null) {
+      website = randomElement(websites)
+      if(parentHost.lenght > 0 && website["url"].includes(parentHost)) {
+        website = null
+      }
+    }
 
     var website = randomElement(websites)
     var element = document.getElementById("website-link")
