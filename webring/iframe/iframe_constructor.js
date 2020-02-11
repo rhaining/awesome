677954ab60f4b45dad3d111abaa5fe1ae54b1f77
Rob Haining <rob@tolar.ws>
2020-02-11 12:01:44 -0500

    frameborder

--- webring/iframe/iframe_constructor.js
+++ webring/iframe/iframe_constructor.js
@@ -1,6 +1,6 @@
 function go() {
   var buffer = "<div style='overflow: hidden;padding-top: 15.625%;position: relative;'>\n"
-  buffer += "\t<iframe style='border: 1; height: 94%;left: 0;position: absolute;top: 0;width:99%;' src='https://kindofawesome.com/webring/iframe/index.html'></iframe>\n"
+  buffer += "\t<iframe style='frameborder: 1; height: 94%;left: 0;position: absolute;top: 0;width:99%;' src='https://kindofawesome.com/webring/iframe/index.html'></iframe>\n"
   buffer += "</div>"
 
   document.write(buffer)
