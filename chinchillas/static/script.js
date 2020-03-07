2380fd1422c2d5816478490399c457506249f197
Rob Haining <rob@tolar.ws>
2020-03-07 15:02:42 -0500

    added custom player

---
+++ chinchillas/static/script.js
@@ -0,0 +1,31 @@
+function loadLatestPodcastEp() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      parsePodcastFeed(this.responseXML)
+    } else if (this.readyState == 4 && this.status == 0){
+      if (window.location.protocol == "file:" && navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        document.getElementById("error").innerHTML = "<p>Looks like a cors error.</p><p>Please update <a href=\"about:config\" target=_blank>about:config</a> to set `privacy.file_unique_origin` to `false`.</p><p>thanks!</p>"
+        document.getElementById("error").style.display = 'block';
+      }
+    }
+
+    // document.getElementById("loading").style.display = 'none'
+  };
+  if (window.location.protocol == "file:") {
+    xhttp.open("GET", "podcast/feed.xml", true)
+  } else {
+    xhttp.open("GET", "https://kindofawesome.com/chinchillas/podcast/feed.xml", true)
+  }
+  xhttp.send();
+}
+
+function parsePodcastFeed(xmlDoc) {
+  episodes = xmlDoc.getElementsByTagName("item")
+  mostRecentEp = episodes[0]
+  enclosure = mostRecentEp.getElementsByTagName("enclosure")[0]
+  url = enclosure.getAttribute("url")
+
+  document.getElementById("podcast-player-source").src = url
+  document.getElementById("podcast-player-audio").load()
+}
