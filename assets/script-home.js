d6e8fb1ea1aeb86d8198d81b70905637ab738b5f
Rob Haining <rob@tolar.ws>
2020-03-10 18:11:30 -0400

    refreshed homepage and tweaked hwere stuff lives

---
+++ assets/script-home.js
@@ -0,0 +1,62 @@
+function fetchProjects() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      populateProjects(data)
+    } else if (this.readyState == 4 && this.status == 0){
+      if (window.location.protocol == "file:" && navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        document.getElementById("error").innerHTML = "<p>Looks like a cors error.</p><p>Please update <a href=\"about:config\" target=_blank>about:config</a> to set `privacy.file_unique_origin` to `false`.</p><p>thanks!</p>"
+        document.getElementById("error").style.display = 'block';
+      }
+    }
+
+    document.getElementById("loading").style.display = 'none'
+  };
+  if (window.location.protocol == "file:") {
+    xhttp.open("GET", "projects.json", true)
+  } else {
+    xhttp.open("GET", "https://kindofawesome.com/projects.json", true)
+  }
+  xhttp.send();
+
+}
+function populateProjects(projects) {
+  var buffer = ""
+  for(var i=0; i<projects.length; i++) {
+    var project = projects[i]
+    var logo = project['logo']
+    var logo_class = null
+    var logo_html = null
+    switch (logo["type"]) {
+      case "img":
+      logo_class = "img-logo"
+      logo_html = `<img src="${logo["img-url"]}"/>`
+        break;
+
+      case "txt":
+      logo_class = "text-logo"
+      logo_html = logo["text"]
+        break;
+      case "html":
+
+      logo_class = "html-logo"
+      logo_html = logo["html"]
+        break;
+
+      default:
+
+    }
+    var html = `
+        <span class="project-section">
+          <div class="${logo_class}">${logo_html}</div>
+          <div class="project-text">
+            <a href="${project['url']}">${project['name']}</a>
+          </div>
+          <div class="project-info">${project['info']}</div>
+        </span>
+    `
+    buffer += html
+  }
+  document.getElementById("project-list").innerHTML = buffer
+}
