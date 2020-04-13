9fd76a34f975d65bc1e6f21efdc0969671a7e03c
Rob Haining <rob@tolar.ws>
2020-04-13 17:46:21 -0400

    project sections on homepage

--- assets/script-home.js
+++ assets/script-home.js
@@ -21,42 +21,54 @@ function fetchProjects() {
   xhttp.send();
 
 }
-function populateProjects(projects) {
+function populateProjects(data) {
   var buffer = ""
-  for(var i=0; i<projects.length; i++) {
-    var project = projects[i]
-    var logo = project['logo']
-    var logo_class = null
-    var logo_html = null
-    switch (logo["type"]) {
-      case "img":
-      logo_class = "img-logo"
-      logo_html = `<img src="${logo["img-url"]}"/>`
-        break;
 
-      case "txt":
-      logo_class = "text-logo"
-      logo_html = logo["text"]
-        break;
-      case "html":
+  for(var i=0; i<data.length; i++){
+    var project_section = data[i]
+    var title = project_section["title"]
+    buffer += `<h6>${title}&hellip;</h6>`
 
-      logo_class = "html-logo"
-      logo_html = logo["html"]
-        break;
+    var projects = project_section["projects"]
 
-      default:
+    for(var j=0; j<projects.length; j++) {
+      var project = projects[j]
 
+      var logo = project['logo']
+      var logo_class = null
+      var logo_html = null
+      switch (logo["type"]) {
+        case "img":
+        logo_class = "img-logo"
+        logo_html = `<img src="${logo["img-url"]}"/>`
+          break;
+
+        case "txt":
+        logo_class = "text-logo"
+        logo_html = logo["text"]
+          break;
+        case "html":
+
+        logo_class = "html-logo"
+        logo_html = logo["html"]
+          break;
+
+        default:
+
+      }
+      var html = `
+          <span class="project-section">
+            <div class="${logo_class}">${logo_html}</div>
+            <div class="project-text">
+              <a href="${project['url']}">${project['name']}</a>
+            </div>
+            <div class="project-info">${project['info']}</div>
+          </span>
+      `
+      buffer += html
     }
-    var html = `
-        <span class="project-section">
-          <div class="${logo_class}">${logo_html}</div>
-          <div class="project-text">
-            <a href="${project['url']}">${project['name']}</a>
-          </div>
-          <div class="project-info">${project['info']}</div>
-        </span>
-    `
-    buffer += html
+
+    buffer += `<div class="float-clear">&nbsp;</div>`
   }
   document.getElementById("project-list").innerHTML = buffer
 }
